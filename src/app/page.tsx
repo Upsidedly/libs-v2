a5fc9f3ddf9234a0c46e14a5b5ec256046668b73
Upsidedly <matthewlloydw@gmail.com>
2023-12-02 23:45:52 -0500

    docs: updated DSharpPlus data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <main className="flex min-h-screen flex-col justify-between p-24 w-[100%]">
         <div className='w-full flex justify-center align-center flex-col'>
           <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl'>Discord API Library Comparisons</h1>
-          <p className='mt-0.5'>By Upsided, Information from <a href="https://libs.advaith.io/" className='text-blue-500 hover:underline underline-offset-4 decoration-1 duration-100'>libs by Advaith</a></p>
+          <p className='mt-0.5'>By Upsided, Information from <a target='_blank' href="https://libs.advaith.io/" className='text-blue-500 hover:underline underline-offset-4 decoration-1 duration-100'>libs by Advaith</a></p>
           <p className="mt-0.5">Github: <a className='text-blue-500 hover:underline underline-offset-4' href="https://github.com/Upsidedly/libs-v2">https://github.com/Upsidedly/libs-v2</a></p>
         </div>
         {languages.map((language) => <div key={language} className='mb-4 mt-5 fw'>
