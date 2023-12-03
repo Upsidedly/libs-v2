c3da4c1683d3f9e641e249d7d55cd3bed721d8c8
Upsidedly <matthewlloydw@gmail.com>
2023-12-03 01:20:54 -0500

    fix: navbar looked bad

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -4,11 +4,11 @@ import Image from "next/image";
 import { Dispatch, SetStateAction } from "react";
 
 export function Nav({ searchState }: { searchState: Dispatch<SetStateAction<string>> }) {
-	return <Navbar className="p-5 backdrop-blur z-10">
+	return <Navbar className="p-5 backdrop-blur z-10 sticky" isBordered isBlurred>
 		<NavbarBrand>
 			<a href="#top"><Image src="/favicon.svg" alt="Logo of books" width={36} height={36} /></a>
 		</NavbarBrand>
-		<NavbarContent className="hidden sm:flex gap-4 justify-right">
+		<NavbarContent className="hidden sm:flex gap-4 justify-between">
 			<NavbarItem>
 				<Input type="search" placeholder="Search for a library" onChange={e => searchState(e.target.value)}></Input>
 			</NavbarItem>
