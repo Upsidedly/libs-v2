6e0b06509565ef64b88fc8c14de9b0c81a022b38
Upsidedly <matthewlloydw@gmail.com>
2023-12-02 21:54:03 -0500

    feat: a bit more responsive now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,11 @@ const languages = [...new Set(libs.map((x) => x.language).sort())]
 
 export default function Home() {
   return (
-    <div className='w-full flex justify-center align-center'>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24 w-[95%]">
+    <div className='w-full flex justify-center'>
+      <main className="flex min-h-screen flex-col justify-between p-24 w-[100%]">
         <div>
           <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl'>Discord API Library Comparisons</h1>
-          <p className='mt-0.5'>By Upsided, Forked from <a href="https://libs.advaith.io/" className='text-blue-500 hover:underline underline-offset-4 decoration-1 duration-200'>libs by Advaith</a></p>
+          <p className='mt-0.5'>By Upsided, Forked from <a href="https://libs.advaith.io/" className='text-blue-500 hover:underline underline-offset-4 decoration-1 duration-100'>libs by Advaith</a></p>
         </div>
         {languages.map((language) => <div key={language} className='mb-4 mt-5 fw'>
           <h2 id={language.toLowerCase()} className='scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0 hover:underline underline-offset-4 decoration-1'><a href={`#${encodeURIComponent(language.toLowerCase())}`}>{language}</a></h2>
